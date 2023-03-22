e1a685a170b99ba217bc7ebf02ba3e194b609488
mikecuoco <mcuoco@ucsd.edu>
2023-03-22 09:08:20 -0700

    add linked reads

--- workflow/scripts/xtea.py
+++ workflow/scripts/xtea.py
@@ -29,31 +29,42 @@ else:
         reptype += get_reptype(rep)
 
 # make full path
-workdir = Path(snakemake.output[0]).parent.parent.parent.resolve()
+workdir = Path(snakemake.output.script[0]).parent.parent.parent.resolve()
 
-with tempfile.NamedTemporaryFile() as bam_list:
-    with tempfile.NamedTemporaryFile() as id_list:
-        # create id and bam list files for xtea
-        bam_list.write(f"{snakemake.wildcards.sample}\t{snakemake.input.bam}".encode())
-        bam_list.seek(0)
-        id_list.write(snakemake.wildcards.sample.encode())
-        id_list.seek(0)
+# make id_list
+with tempfile.NamedTemporaryFile("w", delete=False) as tmpfile:
+    tmpfile.write(snakemake.wildcards.individual)
+    cmd = f"-i {tmpfile.name} "
 
-        # generate runscript
-        shell(
-            "xtea "
-            "-b {bam_list.name} "
-            "-i {id_list.name} "
-            "-r {snakemake.input.fa} "
-            "-g {snakemake.input.gencode} "
-            "-l {snakemake.input.rep_lib} "
-            "-p {workdir} "
-            "-x null "
-            "-f 5907 "
-            "--blacklist {snakemake.input.blacklist} "
-            "-y {reptype} "
-            "-n {snakemake.threads} "
-            "--xtea $CONDA_PREFIX/lib "
-        )
+if "10x" in snakemake.wildcards.platform:
+    with tempfile.NamedTemporaryFile("w", delete=False) as tmpfile:
+        for b, bx in zip(snakemake.input["10x_bam"], snakemake.input["10x_bx_bam"]):
+            tmpfile.write(f"{snakemake.wildcards.individual}\t{b}\t{bx}\n")
+        cmd += f"-x {tmpfile.name} "
+else:
+    cmd += "-x null "
+
+if "illumina" in snakemake.wildcards.platform:
+    with tempfile.NamedTemporaryFile("w", delete=False) as tmpfile:
+        for b in snakemake.input["illumina_bam"]:
+            tmpfile.write(f"{snakemake.wildcards.individual}\t{b}\n")
+        cmd += f"-b {tmpfile.name} "
+else:
+    cmd += "-b null "
+
+shell(
+    "xtea "
+    "{cmd} "
+    "-r {snakemake.input.fa} "
+    "-g {snakemake.input.gencode} "
+    "-l {snakemake.input.rep_lib} "
+    "-p {workdir} "
+    "-f 5907 "
+    "--blacklist {snakemake.input.blacklist} "
+    "-y {reptype} "
+    "-n {snakemake.threads} "
+    "--xtea $CONDA_PREFIX/lib "
+)
+
 
 os.remove("submit_calling_jobs_for_samples.sh")
